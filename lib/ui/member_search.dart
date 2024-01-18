d3692239592f8485668b3933d4fd1eb01fe690bc
akkkhoo <akho6954@gmail.com>
2024-01-18 15:57:50 +0900

    조직원 개발

---
+++ lib/ui/member_search.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:mvoms/utilities/constants.dart';
+import 'package:mvoms/utilities/input_widget.dart';
+
+class MemberSearch extends StatefulWidget {
+
+  final Function searchFunc;
+  const MemberSearch({super.key, required this.searchFunc});
+
+  @override
+  State<StatefulWidget> createState() => _MemberSearchState();
+}
+
+class _MemberSearchState extends State<MemberSearch> with InputWidget {
+  // 검색 키워드
+  String keyword = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return // 검색 창
+    Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: ConstantValues.kColorBlue
+      ),
+      child: ExpansionTile(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+        ),
+        backgroundColor: ConstantValues.kColorBlue01,
+        title: const Text("검색", style: TextStyle(fontSize: 13)),
+        children: [
+          // 검색조건
+          Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
+                  child: Column(
+                    children: [
+                      Row(
+                          children: [
+                            Container(
+                                alignment: Alignment.center,
+                                width: 70,
+                                child: const Text("검색어", style: TextStyle(fontWeight: FontWeight.bold))
+                            ),
+                            Expanded(
+                                child: makeTextCell(
+                                    controller: TextEditingController(text: keyword),
+                                    maxLength: 50,
+                                    decoration: makeInputDecoration(),
+                                    onChanged: (v){
+                                      keyword = v;
+                                    }
+                                )
+                            ),
+                            SizedBox(
+                              width: 80,
+                              child: ElevatedButton(
+                                onPressed: (){
+                                  widget.searchFunc.call(page:0, keyword:keyword);
+                                },
+                                child: const Text("검색"),
+                              ),
+                            )
+                          ]
+                      )
+                    ],
+                  )
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/organization.dart
+++ lib/ui/organization.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mvoms/models/member.dart';
 import 'package:mvoms/models/department.dart';
 import 'package:mvoms/models/pagination.dart';
+import 'package:mvoms/ui/member_search.dart';
 import 'package:mvoms/ui/organization_search.dart';
 import 'package:mvoms/utilities/constants.dart';
 
@@ -23,6 +25,9 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
 
   // 부서 총 개수
 
+  // 조직원 총 명수
+  String _memberCountValue = "";
+
   // REST 요청 관리
   final _rest = RestRepogitory();
 
@@ -32,12 +37,22 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
   // 부서 목록
   late List<Department> _deptRows = [];
 
+  // 조직원 이벤트 목록
+  late List<Member> _memberRows = [];
+  //선언과 동시에 값을 할당하지 않고, 인스턴스가 생성된 후 값을 할당할 수 있는 변수입니다.
+
+  // 페이지 버튼
+  late List<Widget> _pageButtons = [];
   // 기관 페이지 버튼
   late List<Widget> _orgPageButtons = [];
-  
+
   // 부서 페이지 버튼
   late List<Widget> _deptPageButtons = [];
 
+  // 조직원 페이지 버튼
+  late List<Widget> _memberPageButtons = [];
+
+  // 현재 조직 페이지 번호
   // 현재 기관 페이지 번호
   int _organizationCurPageNo = 0;
 
@@ -48,7 +63,11 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
   int _memberCurPageNo = 0;
 
   // 레코드 수
-  final int _recordSize = 10;
+  final int _recordSize = 1;
+
+  // 멤버 레코드 수
+  final int _memberRecordSize = 30;
+
   // 페이지 수
   final int _pageSize = 10;
 
@@ -58,6 +77,9 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
 
     // 기관 목록 가져오기
     getOrganizationList();
+
+    // 조직원 목록 가져오기
+    getMemberList();
 
     // 부서 목록 가져오기
     getDepartmentList();
@@ -369,6 +391,142 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
                         child: Container(
                           color: Colors.white,
                           height: MediaQuery.of(context).size.height,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: Colors.white, // Adjust color for better visibility
+                                  child: Align(
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            const SizedBox(width: 10),
+                                            const Text("총", style: TextStyle(fontSize: 12)),
+                                            Text(
+                                              _memberCountValue,
+                                              style: const TextStyle(
+                                                  fontSize: 12,
+                                                  color: Colors.red,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            const Text("건", style: TextStyle(fontSize: 12)),
+                                            const Spacer(),
+                                            SizedBox(
+                                              width: 150,
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  // TODO: 조직원 탭
+                                                },
+                                                child: const Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: [
+                                                    Icon(Icons.add),
+                                                    Text("조직원 추가")
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+
+                                        // 간격
+                                        const SizedBox(height: 10),
+
+                                        // 조직원 검색 창
+                                        MemberSearch(searchFunc: getMemberList),
+
+                                        // 간격
+                                        const SizedBox(height: 10),
+
+                                        // 조직원 목록 헤더
+                                        Row(
+                                          children: [
+                                            Expanded(
+                                              child: Table(
+                                                border: const TableBorder(
+                                                    bottom: BorderSide(
+                                                        color: ConstantValues.kColorGray, width: 2
+                                                    )
+                                                ),
+                                                columnWidths: getMemberHeaderColWidths(),
+                                                children: [
+                                                  TableRow(
+                                                      children: [
+                                                        makeMemberHeaderCell("소속부서"),
+                                                        makeMemberHeaderCell("아이디"),
+                                                        makeMemberHeaderCell("이름"),
+                                                        makeMemberHeaderCell("이메일"),
+                                                        makeMemberHeaderCell("번호"),
+                                                      ]
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+
+                                        // 조직원 목록
+                                        Expanded(
+                                          child:  ListView.builder(
+                                            itemCount: _memberRows.length,
+                                            itemBuilder: (BuildContext context, int idx) {
+                                              return Container(
+                                                padding: const EdgeInsets.only(top: 3, bottom: 3),
+                                                decoration: const BoxDecoration(
+                                                    border: Border(bottom:BorderSide(color: ConstantValues.kColorGray))),
+                                                child: Row(
+                                                  children: [
+                                                    Expanded(
+                                                      flex: 1,
+                                                      child: Center(
+                                                        child: Text("${_memberRows[idx].department.organization.id}"),
+                                                      ),
+                                                    ),
+                                                    Expanded(
+                                                      flex: 1,
+                                                      child: Center(
+                                                        child: Text("${_memberRows[idx].id}"),
+                                                      ),
+                                                    ),
+                                                    Expanded(
+                                                      flex: 1,
+                                                      child: Center(
+                                                        child: Text(_memberRows[idx].name),
+                                                      ),
+                                                    ),Expanded(
+                                                      flex: 2,
+                                                      child: Center(
+                                                        child: Text(_memberRows[idx].email ?? '-'),
+                                                      ),
+                                                    ),Expanded(
+                                                      flex: 1,
+                                                      child: Center(
+                                                        child: Text(_memberRows[idx].phone ?? '-'),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ),
+
+                                        // 페이지
+                                        SizedBox(
+                                          height: 50,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: _memberPageButtons,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -412,6 +570,23 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
   }
 
   /// 기관 목록 헤더 생성
+  /// 조직원 목록 헤더 간격 조정
+  Map<int, TableColumnWidth> getMemberHeaderColWidths() {
+    return const <int, TableColumnWidth>{
+      // 소속부서
+      0: FlexColumnWidth(1),
+      // 아이디
+      1: FlexColumnWidth(1),
+      // 이름
+      2: FlexColumnWidth(1),
+      // 이메일
+      3: FlexColumnWidth(2),
+      // 번호
+      4: FlexColumnWidth(1),
+    };
+  }
+
+  /// 기관 목록 헤더 생성
   ///
   /// - [label]: 셀 내용
   TableCell makeOrganizationHeaderCell(String label) {
@@ -438,6 +613,20 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
             style: const TextStyle(fontWeight: FontWeight.bold)),
       ),
     );
+  }
+
+  /// 조직원 목록 헤더 생성
+  ///
+  /// - [label]: 셀 내용
+  TableCell makeMemberHeaderCell(String label) {
+    return TableCell(
+        verticalAlignment: TableCellVerticalAlignment.middle,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(label,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+        ));
   }
 
   /// 기관 목록 조회
@@ -490,4 +679,31 @@ class _MVOMSOrganizationState extends State<MVOMSOrganization> with InputWidget 
       // 건수
     });
   }
+
+  /// 조직원 목록 조회
+  ///
+  /// - [page]: 조회된 페이지 번호, 0 부터 시작
+  /// - [keyword]: 검색 조건 키워드
+  void getMemberList({int page = 0, String? orgId, String? depId, String? keyword}) async{
+    // 현재 페이지 저장
+    _memberCurPageNo = page;
+
+    var resMap = await _rest.searchMembers(page: page, size: _memberRecordSize, orgId: orgId, depId: depId, keyword: keyword);
+
+    setState(() {
+      // 목록 업데이트
+      _memberRows.clear();
+
+      for(var content in resMap['content']) {
+        Member member = Member.fromJson(content);
+        _memberRows.add(member);
+      }
+      // 페이징 생성
+      Pagination pg = Pagination.fromJson(resMap);
+      _memberPageButtons = makePageButtons(pg, _pageSize, (pageNum){getMemberList(page:pageNum);});
+      // 건수
+      _memberCountValue = "${pg.totalElements}";
+    });
+  }
+
 }
